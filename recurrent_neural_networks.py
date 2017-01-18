cb300de9cf46938d67c3c175461a9b382b049b1a
anhad <anhad.mohananey@infibeam.net>
2017-01-18 12:09:08 +0530

    code upd

--- recurrent_neural_networks.py
+++ recurrent_neural_networks.py
@@ -1,30 +1,3 @@
-
-# coding: utf-8
-
-# ## Recurrent Neural Networks: Character RNNs with Keras
-#
-# Often we are not interested in isolated datapoints, but rather datapoints within a context of others. A datapoint may mean something different depending on what's come before it. This can typically be represented as some kind of _sequence_ of datapoints, perhaps the most common of which is a time series.
-#
-# One of the most ubiquitous sequences of data where context is especially important is natural language. We have quite a few words in English where the meaning of a word may be totally different depending on it's context. An innocuous example of this is "bank": "I went fishing down by the river bank" vs "I deposited some money into the bank".
-#
-# If we consider that each word is a datapoint, most non-recurrent methods will treat "bank" in the first sentence exactly the same as "bank" in the second sentence - they are indistinguishable. If you think about it, in isolation they are indistinguishable to us as well - it's the same word!
-#
-# We can only start to discern them when we consider the previous word (or words). So we might want our neural network to consider that "bank" in the first sentence is preceded by "river" and that in the second sentence "money" comes a few words before it. That's basically what RNNs do - they "remember" some of the previous context and that influences the output it produces. This "memory" (called the network's "_hidden state_") works by retaining some of the previous outputs and combining it with the current input; this recursing (feedback) of the network's output back into itself is where its name comes from.
-#
-# This recursing makes RNNs quite deep, and thus they can be difficult to train. The gradient gets smaller and smaller the deeper it is pushed backwards through the network until it "vanishes" (effectively becomes zero), so long-term dependencies are hard to learn. The typical practice is to only extend the RNN back a certain number of time steps so the network is still trainable.
-#
-# Certain units, such as the LSTM (long short-term memory) and GRU (gated recurrent unit), have been developed to mitigate some of this vanishing gradient effect.
-#
-# Let's walkthrough an example of a character RNN, which is a great approach for learning a character-level language model. A language model is essentially some function which returns a probability over possible words (or in this case, characters), based on what has been seen so far. This function can vary from region to region (e.g. if terms like "pop" are used more commonly than "soda") or from person to person. You could say that a (good) language model captures the style in which someone writes.
-#
-# Language models often must make the simplifying assumption that only what came immediately (one time step) before matters (this is called the "Markov assumption"), but with RNNs we do not need to make such an assumption.
-#
-# We'll use Keras which makes building neural networks extremely easy (this example is an annotated version of Keras's [LSTM text generation example](https://github.com/fchollet/keras/blob/master/examples/lstm_text_generation.py)).
-#
-# First we'll do some simple preparation - import the classes we need and load up the text we want to learn from.
-
-# In[1]:
-
 import random
 import numpy as np
 from glob import glob
@@ -45,11 +18,10 @@ unique_tokens_list = set(token_list)
 print "Unique corpus tokens: "+str(len(unique_tokens_list))
 corpus_file.close()
 # Parameters to run the script :
-epochs = 60  # define the number of epochs to fit the model on
+epochs = 60  # no epochs
 batch_size_value = 128 #  values could be from 120 to len(token_list) - choosing 128 as default for running while on cpu
 max_len = 20 # set a fixed vector size so that we can look at specific windows of characters
 
-# In[3]:
 # defining the model on keras
 model_rnn = Sequential()
 model_rnn.add(LSTM(512, return_sequences=True, input_shape=(max_len, len(unique_tokens_list))))
@@ -59,49 +31,6 @@ model_rnn.add(Dropout(0.2))
 model_rnn.add(Dense(len(unique_tokens_list)))
 model_rnn.add(Activation('softmax'))
 model_rnn.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-
-
-# We're framing our task as a classification task. Given a sequence of characters, we want to predict the next character. We equate each character with some label or category (e.g. "a" is 0, "b" is 1, etc).
-#
-# We use the _softmax_ activation function on our output layer - this function is used for categorical output. It turns the output into a probability distribution over the categories (i.e. it makes the values the network outputs sum to 1). So the network will essentially tell us how strongly it feels about each character being the next one.
-#
-# The categorical cross-entropy loss the standard loss function for multilabel classification, which basically penalizes the network more the further off it is from the correct label.
-#
-# We use dropout here to prevent overfitting - we don't want the network to just return things already in the text, we want it to have some wiggle room and create novelty! Dropout is a technique where, in training, some percent (here, 20%) of random neurons of the associated layer are "turned off" for that epoch. This prevents overfitting but preventing the network from relying on particular neurons.
-#
-# That's it for the network architecture!
-#
-# To train, we have to do some additional preparation. We need to chop up the text into character sequences of the length we specified (`max_len`) - these are our training inputs. We match them with the character that immediately follows each sequence. These are our expected training outputs.
-#
-# For example, say we have the following text (this quote is from Zhuang Zi). With `max_len=20`, we could manually create the first couple training examples like so:
-
-# In[11]:
-
-example_text = "The fish trap exists because of the fish. Once you have gotten the fish you can forget the trap. The rabbit snare exists because of the rabbit. Once you have gotten the rabbit, you can forget the snare. Words exist because of meaning. Once you have gotten the meaning, you can forget the words. Where can I find a man who has forgotten words so that I may have a word with him?"
-
-# step size here is 3, but we can vary that
-input_1 = example_text[0:20]
-true_output_1 = example_text[20]
-# >>> 'The fish trap exists'
-# >>> ' '
-
-input_2 = example_text[3:23]
-true_output_2 = example_text[23]
-# >>> 'fish trap exists be'
-# >>> 'c'
-
-input_3 = example_text[6:26]
-true_output_3 = example_text[26]
-# >>> 'sh trap exists becau'
-# >>> 's'
-
-# etc
-
-
-# We can generalize this like so:
-
-# In[4]:
-
 step = 1
 inputs = []
 outputs = []
@@ -156,30 +85,6 @@ example_char_labels = {
     'd': 3,
     ' ' : 4
 }
-
-# matrix form
-# the example uses only five kinds of characters,
-# so the vectors only need to have five components,
-# and since the input phrase has seven characters,
-# the matrix has seven vectors.
-[
-    [0, 0, 1, 0, 0], # c
-    [1, 0, 0, 0, 0], # a
-    [0, 1, 0, 0, 0], # b
-    [0, 0, 0, 0, 1], # (space)
-    [0, 0, 0, 1, 0], # d
-    [1, 0, 0, 0, 0], # a
-    [0, 1, 0, 0, 0]  # b
-]
-
-
-# That matrix represents a _single_ training example, so for our full set of training examples, we'd have a stack of those matrices (hence a 3-tensor).
-#
-# ![A 3-tensor of training examples](../assets/rnn_3tensor.png)
-#
-# And the outputs for each example are each a one-hot vector (i.e. a single character). With that in mind:
-
-# In[19]:
 
 # using bool to reduce memory usage
 X = np.zeros((len(inputs), max_len, len(unique_tokens_list)), dtype=np.bool)
@@ -242,13 +147,6 @@ def sample(probs, temperature):
     choices = range(len(probs))
     return np.random.choice(choices, p=dist)
 
-
-# The _temperature_ controls how random we want the network to be. Lower temperatures favors more likely values, whereas higher temperatures introduce more and more randomness. At a high enough temperature, values will be chosen at random.
-#
-# With this generation function we can modify how we train the network so that we see some output at each step:
-
-# In[21]:
-
 for i in range(epochs):
     print('epoch %d'%i)
     # set nb_epoch to 1 since we're iterating manually
@@ -260,5 +158,3 @@ for i in range(epochs):
         print('temperature: %0.2f'%temp)
         print('%s'%generate(temperature=temp))
 
-
-# That's about all there is to it.
